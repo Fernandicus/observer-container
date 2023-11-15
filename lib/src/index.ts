aeedc26b3d1f9f6c1c2d73e1ad41040249107fd9
Fernando G <fernanprojects@gmail.com>
2023-11-15 11:06:10 +0100

    Refactorize index.js

--- lib/src/index.ts
+++ lib/src/index.ts
@@ -1,26 +1,20 @@
-import { BuildObservers } from "./observers-container/application/BuildObservers";
+import { buildSubject, observerCreator } from "./injection-container";
 import { BuildSubject } from "./observers-container/application/BuildSubject";
-import { CreateObserver } from "./observers-container/application/CreateObserver";
-import { ObserverContainer } from "./observers-container/domain/ObserverContainer";
-import { SubjectsMap } from "./observers-container/domain/SubjectsMap";
 
-type LoadObserversProps = Parameters<BuildObservers["build"]>[number];
-type AddObserversProps = Parameters<BuildObservers["build"]>[number];
+type AddObserver<T> = ObserverTags & {
+  observers: ReturnType<typeof createObserver<T>>[];
+};
+type LoadObservers = Prop.LoadObserver[];
+type ReturnObservers = ReturnType<BuildSubject["withObserversLoaders"]>;
 
-const subjectsMap = new SubjectsMap();
-const observerContainer = new ObserverContainer({ subjectsMap });
-const observerCreator = new CreateObserver(observerContainer);
-const buildSubject = new BuildSubject(observerContainer);
-const observersBuilder = new BuildObservers();
-
-export const createObserver = <T>(onUpdate: (data: T) => void) => {
+export function createObserver<T>(onUpdate: (data: T) => void) {
   return observerCreator.create(onUpdate);
-};
+}
 
-export const loadObservers = (props: LoadObserversProps) => {
+export function loadObservers(props: LoadObservers): ReturnObservers {
   return buildSubject.withObserversLoaders(props);
-};
+}
 
-export const addObservers = (props: AddObserversProps) => {
-  return observersBuilder.build(props);
-};
+export function addObservers<T>(observers: AddObserver<T>[]): AddObserver<T>[] {
+  return observers;
+}
