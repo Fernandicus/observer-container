e7a4a34f9e606830f858e3fce00a573c68528fe5
Fernando G <fernanprojects@gmail.com>
2023-12-02 11:56:32 +0100

    Fix: jest.config from cjs to ems

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,7 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
+export default {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  testMatch: ['<rootDir>/__tests__/**/*.test.ts'],
+  testPathIgnorePatterns: ['/node_modules/'],
 };
